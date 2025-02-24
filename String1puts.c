d96e4343f8d121f99c16e788e91a24fc729616f2
Kundanakonduru <kondurukundana1@gmail.com>
2025-02-24 20:28:45 +0530

    Create String1puts.c

---
+++ String1puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char name[23];
+    printf("Enter string:");
+    scanf("%s",name);
+    printf("%s\n",name);
+    puts(name);
+    puts(name);
+    return 0;
+}
